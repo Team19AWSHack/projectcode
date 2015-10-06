4d3dfca8336411a7bf81acadea0304965fe7d3a9
Michael Newman <newmaniese@gmail.com>
2015-10-06 16:37:44 -0700

    Adding a receiver API

--- webhook/app.py
+++ webhook/app.py
@@ -16,13 +16,16 @@ request_table = Table("VaccineRequest")
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 GOOGLE_REQUEST = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s"
 
+RAPIDPRO_API_KEY = os.environ.get("RAPIDPRO_API_KEY")
+GIVER_FLOW_UUID = "ff270149-ee71-4aa8-99c4-4fe13d748a43"
+
 def normalize_location(loc_string):
     print GOOGLE_REQUEST % (loc_string, GOOGLE_API_KEY)
     res = requests.get(GOOGLE_REQUEST % (loc_string, GOOGLE_API_KEY))
     return res.json()
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
+@app.route("/location", methods=["POST"])
+def location():
     values = request.form.get("values")
 
     loc = normalize_location(request.form.get("text"))
@@ -31,7 +34,6 @@ def webhook():
         return Response(json.dumps({ "status" : "fail", "location" : False }), mimetype="application/json")
 
     data = {
-        "values" : values,
         "location" : {
             "lat" : str(loc['results'][0]['geometry']['location']['lat']),
             "lon" : str(loc['results'][0]['geometry']['location']['lng']),
@@ -42,10 +44,46 @@ def webhook():
     d_data = data.copy()
     d_data['phone'] = request.form.get("phone")
     d_data['time'] = int(time.time())
-    request_table.put_item(data=d_data)
+    request_table.put_item(data=d_data, overwrite=True)
 
     data['status'] = "success"
     return Response(json.dumps(data), mimetype="application/json")
+
+def get_rapidpro_contact_info(phone):
+    res = requests.get("https://api.rapidpro.io/api/v1/contacts.json?phone=%2B12482198946", headers={
+        "Authorization" : "Token %s" % RAPIDPRO_API_KEY
+    })
+    return res.json()['results'][0]
+
+@app.route("/receiver", methods=["POST"])
+def receiver():
+    phone = request.form.get("phone")
+    receiver = request_table.get_item(phone=phone)
+    receiver['type'] = "receiver"
+    receiver.save()
+    loc = normalize_location("%s, %s" % (receiver['location']['lat'], receiver['location']['lon']))
+    #TODO: Query for closest locations
+    payload = {
+        "flow_uuid": GIVER_FLOW_UUID,
+        "phone": [
+            "+7173327758"
+        ],
+        "extra": {
+            "location": receiver['location'],
+            "location_english" : loc['results'][0]['formatted_address'],
+            "receiver_phone" : phone
+        }
+    }
+    res = requests.post("https://api.rapidpro.io/api/v1/runs.json", headers={
+        "Authorization" : "Token %s" % RAPIDPRO_API_KEY
+    }, data=payload)
+    return Response(json.dumps({"status" : "success"}))
+
+@app.route("/giver", methods=["POST"])
+def giver():
+    phone = request.form.get("phone")
+    giver = request_table.get_item(phone=phone)
+    giver['type'] = "giver"
 
 if __name__ == "__main__":
     app.debug = True
